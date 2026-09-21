c33fca383ae79840253782ccd762282ce3187a65
josephbubenshi8-rgb <josephbubenshi8@gmail.com>
2026-09-21 12:33:57 +0200

    Add admin endpoint to import starter dictionary pack

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import fs from "node:fs/promises";
+import path from "node:path";
 import cors from "cors";
 import dotenv from "dotenv";
 import bcrypt from "bcryptjs";
